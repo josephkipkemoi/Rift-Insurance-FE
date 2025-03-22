dcc45f748756d639e999fa8066473313aae32c34
josephkipkemoi <jkemboe@gmail.com>
2025-03-22 17:47:43 +0000

    fix landingpage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,5 @@
 import HeaderComponent from '../components/HeaderComponent/HeaderComponent';
 import LandingComponent from '../components/LandingComponent/LandingComponent';
-import './App.css';
-
-// set page title
-const webName = process.env.REACT_APP_TITLE
-document.title = `${webName} | Home`
 
 const LandingPage = () => {
   return (
