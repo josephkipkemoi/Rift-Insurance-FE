edbf67b808eb08918a7d530e7eebf9038530c36b
josephkipkemoi <jkemboe@gmail.com>
2025-03-22 17:47:03 +0000

    Contact page

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/HeaderComponent/HeaderComponent"
+
+const ContactPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>Contact Us</h1>
+        </div>
+    )
+}
+
+export default ContactPage
