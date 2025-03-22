e952123b1d92cdeff1211a72615f01a3b3f940df
josephkipkemoi <jkemboe@gmail.com>
2025-03-22 17:34:27 +0000

    made Header component responsive'

--- src/components/HeaderComponent/HeaderComponent.js
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -1,23 +1,47 @@
 import { Link } from "react-router-dom"
+
+import Logo from "../../images/logo.png"
+
 import "./HeaderComponent.css"
 
 const HeaderComponent = () => {
     return (
-        <header>
-             <h1>
-                <Link to="/">Rift Insurance</Link>
-            </h1>
-            <nav>               
-                <Link to="/">Home</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/testimonials">Testimonials</Link>
-                <Link to="/contact-us">Contact Us</Link>
-                <Link to="/about-us">About Us</Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light rift-nav-bar">
+            <Link className="navbar-brand" to="/">
+                <img src={Logo} alt="Rift-Insurance-Logo" className="img-fluid" />
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/services">Services</Link>
+                    </li>                  
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about-us">About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact-us">Contact</Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <Link className="dropdown-item" href="#">Action</Link>
+                            <Link className="dropdown-item" href="#">Another action</Link>
+                            <div className="dropdown-divider"></div>
+                            <Link className="dropdown-item" href="#">Something else here</Link>
+                        </div>
+                    </li>              
+                </ul>
+            </div>
             </nav>
-            <nav className="rift-login">
-                <Link href="#">Login</Link>
-            </nav>
-        </header>
     )
 }
 
