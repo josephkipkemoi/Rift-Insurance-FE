f2abc6237d71bea8d30ccea6488a4890931410bc
josephkipkemoi <jkemboe@gmail.com>
2025-03-22 17:46:48 +0000

    Services Page

---
+++ src/pages/ServicePage.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/HeaderComponent/HeaderComponent"
+
+const ServicePage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>Services</h1>
+        </div>
+    )
+}
+
+export default ServicePage
