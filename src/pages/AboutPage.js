a29e080bb912ec116f2bc9eb612eb12f5094c8cc
josephkipkemoi <jkemboe@gmail.com>
2025-03-22 17:47:56 +0000

    About us page

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/HeaderComponent/HeaderComponent"
+
+const AboutPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>About Us</h1>
+        </div>
+    )
+}
+
+export default AboutPage
